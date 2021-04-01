2378b2156e845148f39267e2feba66021caf375d
akburaevam <akburaevam@gmail.com>
2021-04-01 20:33:57 +0600

    lesson10

--- lesson10.dart
+++ lesson10.dart
@@ -1,14 +1,14 @@
 import 'dart:io';
 import 'dart:math';
-void main (){
+
+void main() {
   task1();
 }
-void task1 () {
+
+void task1() {
   print('enter the num');
   var a = stdin.readLineSync()!;
   List<String> b = a.split('');
-  final List<int> c = b.map((e)=>int.parse(e)).toList(); 
-  //print(c);
-  print(c.isEmpty ? 0 : c.reduce(max)); //prints 0
-
+  final List<int> c = b.map((e) => int.parse(e)).toList();
+  print(c.isEmpty ? 0 : c.reduce(max));
 }
